75dd618f638518f7852770d6f16d965b4ece9211
Michon van Dooren <michon1992@gmail.com>
2017-12-11 12:20:08 +0000

    Day 4 part 2 + test cases

--- AOC04.hs
+++ AOC04.hs
@@ -1,13 +1,21 @@
-import Data.List (nub)
+module AOC04 where
+import Data.List (nub, sort)
 
-isValid :: [String] -> Int
-isValid [] = 0
-isValid (words) = if words == nub words then 1 else 0
+-- Check whether a given list has duplicates
+hasDuplicates :: [String] -> Bool
+hasDuplicates [] = False
+hasDuplicates (words) = words /= nub words
 
-main' :: [String] -> Int
-main' [] = 0
-main' (line:rest) = (isValid (words line)) + (main' rest)
+-- Get the solution for part 1
+partOne :: [[String]] -> Int
+partOne phrases = length (filter (not . hasDuplicates) phrases)
+
+-- Get the solution for part 2
+partTwo :: [[String]] -> Int
+partTwo phrases = length (filter (not . hasDuplicates) (map (map sort) phrases))
 
 main = do
    input <- getContents
-   putStrLn (show (main' (lines input)))
+   let phrases = map words (lines input)
+   putStrLn (show (partOne phrases))
+   putStrLn (show (partTwo phrases))
---
+++ AOC04Spec.hs
@@ -0,0 +1,22 @@
+module AOC04Spec where
+import Test.Hspec
+import AOC04 (hasDuplicates, partOne, partTwo)
+
+main :: IO ()
+main = hspec $ do
+   describe "hasDuplicates" $ do
+      it "returns false for an empty list" $ hasDuplicates [] `shouldBe` False
+      it "returns false for a list without duplicates" $ hasDuplicates ["aa", "bb"] `shouldBe` False
+      it "returns true for a list with duplicates" $ hasDuplicates ["aa", "aa", "bb"] `shouldBe` True
+
+   describe "partOne" $ do
+      it "returns the correct value for aa bb cc dd ee" $ partOne [words "aa bb cc dd ee"] `shouldBe` 1
+      it "returns the correct value for aa bb cc dd aa" $ partOne [words "aa bb cc dd aa"] `shouldBe` 0
+      it "returns the correct value for aa bb cc dd aaa" $ partOne [words "aa bb cc dd aaa"] `shouldBe` 1
+
+   describe "partTwo" $ do
+      it "returns the correct value for abcde fghij" $ partTwo [words "abcde fghij"] `shouldBe` 1
+      it "returns the correct value for abcde xyz ecdab" $ partTwo [words "abcde xyz ecdab"] `shouldBe` 0
+      it "returns the correct value for a ab abc abd abf abj" $ partTwo [words "a ab abc abd abf abj"] `shouldBe` 1
+      it "returns the correct value for iiii oiii ooii oooi oooo" $ partTwo [words "iiii oiii ooii oooi oooo"] `shouldBe` 1
+      it "returns the correct value for oiii ioii iioi iiio" $ partTwo [words "oiii ioii iioi iiio"] `shouldBe` 0
