52eb1c448779d5339f8d5d4b7c4d23d7123ba87e
Michon van Dooren <michon1992@gmail.com>
2017-12-18 12:26:27 +0000

    Updated the gen script to include parsec stuff

--- gen.sh
+++ gen.sh
@@ -3,6 +3,18 @@
 cat <<EOF > AOC$1.hs
 module AOC$1 where
 import Common (numbers)
+import Common.Megaparsec (Parser, parseE', (<||>))
+import Control.Applicative ((<*), (*>))
+import qualified Text.Megaparsec as P
+import qualified Text.Megaparsec.Char as C
+
+-- Data structure for data
+data Data = Data String
+
+-- Parse a line into a Data
+matchData = Data <$> P.some C.anyChar :: Parser Data
+parseLine :: String -> Data
+parseLine = parseE' matchData
 
 -- Get the solution for part 1
 partOne :: [Int] -> Int
@@ -14,20 +26,26 @@ partTwo digits = 0
 
 main = do
    -- input <- getContents
-   -- let digits = map numbers (map words (lines input))
    input <- getLine
    let digits = numbers (words input)
+   let data = parseLine input
    putStrLn (show (partOne digits))
    putStrLn (show (partTwo digits))
 EOF
 
 cat <<EOF > AOC$1Spec.hs
 module AOC$1Spec where
+import AOC$1 (matchData, partOne, partTwo)
+import Common.Megaparsec (parseE)
 import Test.Hspec
-import AOC$1 (partOne, partTwo)
+import Test.Hspec.Megaparsec
 
 main :: IO ()
 main = hspec $ do
+   describe "matchData" $ do
+      it "parses anything" $ parseE matchEscape "foo" \`shouldParse\` "foo"
+      it "does not parse an empty string" $ parseE matchEscape \`shouldFailOn\` ""
+
    describe "partOne" $ do
       it "returns the correct value for [1, 2, 3]" $ partOne [1, 2, 2] \`shouldBe\` 0
 
