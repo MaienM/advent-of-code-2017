43f74afa73b00dedd2a51014ebac28b7aa8baa99
Michon van Dooren <michon1992@gmail.com>
2017-12-19 11:01:49 +0000

    Day 10 part 2

--- AOC10.hs
+++ AOC10.hs
@@ -2,13 +2,21 @@ module AOC10 where
 import Common (shift)
 import Common.Megaparsec (Parser, parseE', symbol)
 import Control.Applicative ((<*), (*>))
+import Data.Bits (xor)
+import Data.Char (ord)
+import Data.List.Split (chunksOf)
+import Numeric (showHex)
 import qualified Text.Megaparsec as P
 import qualified Text.Megaparsec.Char.Lexer as L
 
 -- Parse a line into a list of ints
-matchLine = P.sepBy L.decimal (symbol ",") :: Parser [Int]
-parseLine :: String -> [Int]
-parseLine = parseE' matchLine
+matchLineOne = P.sepBy L.decimal (symbol ",") :: Parser [Int]
+parseLineOne :: String -> [Int]
+parseLineOne = parseE' matchLineOne
+
+-- Parse a line into a list of bytes
+parseLineTwo :: String -> [Int]
+parseLineTwo = map ord
 
 -- Reverse a portion of a list, with wrap around
 reverseSlice :: Int -> Int -> [a] -> [a]
@@ -16,28 +24,42 @@ reverseSlice start len list = do
    let (a, b) = splitAt len (shift list (-start))
    shift ((reverse a) ++ b) start
 
--- Apply the 'hash'
-hash' :: Int -> Int -> [a] -> [Int] -> [a]
-hash' _ _ nums [] = nums
-hash' pos skip nums (length:lengths) = hash' (pos+skip+length) (skip+1) (reverseSlice pos length nums) lengths
-hash :: [Int] -> [Int] -> [Int]
-hash = hash' 0 0
+-- Calculate the sparse hash
+sparse' :: Int -> Int -> [a] -> [Int] -> [a]
+sparse' _ _ nums [] = nums
+sparse' pos skip nums (length:lengths) = sparse' (pos+skip+length) (skip+1) (reverseSlice pos length nums) lengths
+sparse :: [Int] -> [Int] -> [Int]
+sparse = sparse' 0 0
+
+-- Convert the sparse hash to the dense hash
+dense :: [Int] -> [Int]
+dense sparse = map (foldl xor 0) (chunksOf 16 sparse)
+
+-- Convert a hash into a hexadecimal string
+hexToTwoChar :: Int -> String
+hexToTwoChar byte
+   | byte < 16 = '0':((showHex byte) "")
+   | otherwise = (showHex byte) ""
+hex :: [Int] -> String
+hex hash = concat (map hexToTwoChar hash)
 
 -- Get the solution for part 1
 partOne' :: [Int] -> [Int] -> Int
-partOne' lengths nums = do
-   let (x:y:_) = hash lengths nums
+partOne' nums lengths = do
+   let (x:y:_) = sparse nums lengths
    x * y
 partOne :: [Int] -> Int
 partOne = partOne' [0..255]
 
 -- Get the solution for part 2
-partTwo :: [Int] -> Int
-partTwo lengths = 0
+partTwo :: [Int] -> String
+partTwo lengths = do
+   let lengths' = concat (replicate 64 (lengths ++ [17, 31, 73, 47, 23]))
+   let sparse' = sparse [0..255] lengths'
+   let dense' = dense sparse'
+   hex dense'
 
 main = do
-   -- input <- getContents
    input <- getLine
-   let lengths = parseLine input
-   putStrLn (show (partOne lengths))
-   putStrLn (show (partTwo lengths))
+   putStrLn (show (partOne (parseLineOne input)))
+   putStrLn (partTwo (parseLineTwo input))
--- AOC10Spec.hs
+++ AOC10Spec.hs
@@ -1,13 +1,16 @@
 module AOC10Spec where
-import AOC10 (matchLine, reverseSlice, hash, partOne', partTwo)
+import AOC10 (matchLineOne, parseLineTwo, reverseSlice, sparse, dense, hexToTwoChar, hex, partOne', partTwo)
 import Common.Megaparsec (parseE)
 import Test.Hspec
 import Test.Hspec.Megaparsec
 
 main :: IO ()
 main = hspec $ do
-   describe "matchLine" $ do
-      it "parses a comma-separated list of numbers" $ parseE matchLine "1, 2" `shouldParse` [1, 2]
+   describe "matchLineOne" $ do
+      it "parses a comma-separated list of numbers" $ parseE matchLineOne "1, 2" `shouldParse` [1, 2]
+
+   describe "parseLineTwo" $ do
+      it "returns the correct value for the example" $ parseLineTwo "1,2,3" `shouldBe` [49, 44, 50, 44, 51]
 
    describe "reverseSlice" $ do
       it "returns the correct value for 0 2 [0..4]" $ reverseSlice 0 2 [0..4] `shouldBe` [1, 0, 2, 3, 4]
@@ -21,11 +24,27 @@ main = hspec $ do
       it "returns the correct value for 3 3 [0..4]" $ reverseSlice 3 3 [0..4] `shouldBe` [3, 1, 2, 0, 4]
       it "returns the correct value for 4 3 [0..4]" $ reverseSlice 4 3 [0..4] `shouldBe` [0, 4, 2, 3, 1]
 
-   describe "hash'" $ do
-      it "returns the correct value for the example" $ hash [0..4] [3, 4, 1, 5] `shouldBe` [3, 4, 2, 1, 0]
+   describe "sparse'" $ do
+      it "returns the correct value for the example" $ sparse [0..4] [3, 4, 1, 5] `shouldBe` [3, 4, 2, 1, 0]
+
+   describe "dense" $ do
+      it "returns the correct value for the example" $ dense (concat (replicate 4 [65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22])) `shouldBe` [64, 64, 64, 64]
+
+   describe "hexToTwoChar" $ do
+      it "returns the correct value for 4" $ hexToTwoChar 4 `shouldBe` "04"
+      it "returns the correct value for 9" $ hexToTwoChar 9 `shouldBe` "09"
+      it "returns the correct value for 10" $ hexToTwoChar 10 `shouldBe` "0a"
+      it "returns the correct value for 15" $ hexToTwoChar 15 `shouldBe` "0f"
+      it "returns the correct value for 16" $ hexToTwoChar 16 `shouldBe` "10"
+
+   describe "hex" $ do
+      it "returns the correct value for [64, 7, 255]" $ hex [64, 7, 255] `shouldBe` "4007ff"
 
    describe "partOne'" $ do
       it "returns the correct value for the example" $ partOne' [0..4] [3, 4, 1, 5] `shouldBe` 12
 
    describe "partTwo" $ do
-      it "returns the correct value for [1, 2, 3]" $ partTwo [1, 2, 2] `shouldBe` 0
+      it "returns the correct value for an empty string" $ partTwo (parseLineTwo "") `shouldBe` "a2582a3a0e66e6e86e3812dcb672a272"
+      it "returns the correct value for 'AoC 2017'" $ partTwo (parseLineTwo "AoC 2017") `shouldBe` "33efeb34ea91902bb2f59c9920caa6cd"
+      it "returns the correct value for '1,2,3'" $ partTwo (parseLineTwo "1,2,3") `shouldBe` "3efbe78a8d82f29979031a4aa0b16a9d"
+      it "returns the correct value for '1,2,4'" $ partTwo (parseLineTwo "1,2,4") `shouldBe` "63960835bcdc130f0b66d7ff4f6a5a8e"
